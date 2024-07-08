573cf32d91bdbe1ff23e82e5c1e49d043be263b7
unknown <leegj420@gmail.com>
2024-07-08 12:04:52 +0900

    2-04 엔티티로 테이블 매핑하기

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,35 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+// 엔티티에는 원래 @Setter를 사용하지 않는다!
+// 원활한 설명을 위해서 책에서는 사용하기로 함
+@Getter
+@Setter
+@Entity
+public class Answer {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,37 @@
+package com.mysite.sbb;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+// 엔티티에는 원래 @Setter를 사용하지 않는다!
+// 원활한 설명을 위해서 책에서는 사용하기로 함
+@Getter
+@Setter
+@Entity
+public class Question {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList;
+}
