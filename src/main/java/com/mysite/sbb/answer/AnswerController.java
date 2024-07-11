f3ebd25e2ba3ea6938414628058cc221d6777011
unknown <leegj420@gmail.com>
2024-07-11 17:00:11 +0900

    3-11 앵커 기능 추가하기

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -46,8 +46,8 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
 
@@ -89,7 +89,7 @@ public class AnswerController {
         }
         
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
 
@@ -118,6 +118,6 @@ public class AnswerController {
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
 
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 }
