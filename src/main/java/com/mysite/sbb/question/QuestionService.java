bbf8193ab06f10c7fae66e26cf7846ba60736a2c
DESKTOP-B3EHO2L\xtcse <leegj420@gmail.com>
2024-07-12 23:46:59 +0900

    3-13 검색 기능 추가하기

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -46,12 +46,12 @@ public class QuestionService {
     }
 
 
-    public Page<Question> getList(int page){
+    public Page<Question> getList(int page, String kw){
 
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        return this.questionRepository.findAll(pageable);
+        return this.questionRepository.findAllByKeyword(kw, pageable);
     }
 
 
