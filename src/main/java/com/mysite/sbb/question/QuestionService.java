3be65877346d87dabdd50fc831d09e68ae9470a7
unknown <leegj420@gmail.com>
2024-07-12 13:19:27 +0900

    3-13 검색기능 추가하기 ~302p

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -7,11 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
 
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -19,6 +22,29 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
     
     private final QuestionRepository questionRepository;
+
+
+    private Specification<Question> search(String kw){
+
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb){
+
+                query.distinct(true);
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or(cb.like(q.get("subject"), "%" + kw + "%"),
+                    cb.like(q.get("content"), "%" + kw + "%"),
+                    cb.like(u1.get("username"), "%" + kw + "%"),
+                    cb.like(a.get("content"), "%" + kw + "%"),
+                    cb.like(u2.get("username"), "%" + kw + "%"));
+            }
+        };
+    }
+
 
     public Page<Question> getList(int page){
 
